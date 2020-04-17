390380738a22fece721dbc562b36b1fd76f181a5
Michael Fekadu <mike.fekadu@gmail.com>
2020-04-17 16:06:51 -0700

    reorganize code

--- clubs.py
+++ clubs.py
@@ -65,21 +65,13 @@ Resources:
     * docopt is cool
         * http://docopt.org
 """
-import re
-
 import pandas as pd
-from colorama import init
+import spacy
 from docopt import docopt
-from termcolor import colored
-from colors import strip_color
 
 from ntfp.ntfp import filter_string_by_relevance, transformer
 from ntfp.ntfp_types import Context, Question
-
-import spacy
-
-# use Colorama to make Termcolor work on Windows too
-init()
+from utils.terminal_colors import green_bold, print_colored_doc, yellow_bold
 
 
 def make_sents(club):
@@ -123,80 +115,13 @@ def make_sents(club):
     return final_sents
 
 
-def green_bold(s: str) -> str:
-    return colored(s, "green", "on_grey", attrs=["bold"])
-
-
-def yellow_bold(s: str) -> str:
-    return colored(s, "yellow", "on_grey", attrs=["bold"])
-
-
-def white_bold(s: str) -> str:
-    return colored(s, "white", "on_grey", attrs=["bold"])
-
-
-def red_bold(s: str) -> str:
-    return colored(s, "red", "on_grey", attrs=["bold"])
-
-
-def grey_out(s: str) -> str:
-    return colored(s, "grey", "on_grey")
-
-
-def print_colored_doc():
-    colored_doc = __doc__
-    to_color_green_bold = (
-        "clubs.py",
-        "(--example | -e)",
-        "(--make-doc | -m)",
-        "(-h | --help)",
-    )
-    to_color_yellow_bold = (
-        "[IN_TXT_FILE]",
-        "[IN_CSV_FILE]",
-        "[OUT_TXT_FILE]",
-    )
-    to_color_white_bold = (
-        "Ask questions about Cal Poly clubs.",
-        "Usage:",
-        "Options:",
-        "Resources:",
-        "Example:",
-    )
-    to_color_white_bold_patterns = (r"(\$.*)",)
-    to_color_red_bold_patterns = (r"(defaults to.*)",)
-    to_color_grey_out = ("[//]: # (markdown comment # noqa)",)
-
-    def repl(color_fun, strip=True):
-        def _repl(m):
-            s = m.group(1)
-            s = strip_color(s) if strip else s
-            return f"{color_fun(s)}"
-
-        return _repl
-
-    for s in to_color_yellow_bold:
-        colored_doc = colored_doc.replace(s, yellow_bold(s))
-    for s in to_color_green_bold:
-        colored_doc = colored_doc.replace(s, green_bold(s))
-    for s in to_color_white_bold:
-        colored_doc = colored_doc.replace(s, white_bold(s))
-    for s in to_color_grey_out:
-        colored_doc = colored_doc.replace(s, grey_out(s))
-    for s in to_color_red_bold_patterns:
-        colored_doc = re.sub(s, repl(red_bold), colored_doc)
-    for s in to_color_white_bold_patterns:
-        colored_doc = re.sub(s, repl(white_bold), colored_doc)
-    print(colored_doc)
-
-
 if __name__ == "__main__":
     arguments = docopt(__doc__, version="Clubs 1.0", help=False)
     VERBOSE = arguments["--verbose"]
     DEBUG = arguments["--debug"]
     print(arguments) if DEBUG else None
     if arguments["--help"]:
-        print_colored_doc()
+        print_colored_doc(__doc__)
         exit()
     IN_CSV_FILE = arguments["IN_CSV_FILE"] or "clubs.csv"
     IN_TXT_FILE = arguments["IN_TXT_FILE"] or "clubs.txt"
---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+"""
+Utilities for the main demo code
+"""
